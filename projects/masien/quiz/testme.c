9e8562b6534d94594e14381fcf038816bfa3109a
masien <nick.masie@gmail.com>
2018-02-04 14:37:39 -0600

    Create testme.c

---
+++ projects/masien/quiz/testme.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+	
+	// generate a random char in ASCII range 32-126
+	char randChar = (rand() % (127 - 32)) + 32;
+	
+    return randChar;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+	
+	char str[6];
+	char randChar;
+	int randNum = 0;
+	
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		// generate a random lowercase letter 'e' - 't'
+		randNum = (16 * (rand() / (RAND_MAX + 1.0)) + 101);
+		randChar = (char) randNum;
+		str[i] = randChar;
+	}
+	// add terminating 0 to end of string
+	str[i] = '\0';
+	
+    return str;
+}
+
+void testme()
+{
+	clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+	
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error\n");
+	  
+	  end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time: %lf\n", cpu_time_used);
+	  
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
+    testme();
+	 
+    return 0;
+}
