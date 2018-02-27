9946b2bd88839cf678b6516fa3379a1b35d45b2f
masien <nick.masie@gmail.com>
2018-02-26 23:45:38 -0600

    Update randomtestcard2.c

--- projects/masien/dominion/randomtestcard2.c
+++ projects/masien/dominion/randomtestcard2.c
@@ -73,7 +73,7 @@ int main()
 			printf("testG.hand[0][%d] = (%d)\n", ii, testG.hand[curPlayer][ii]);
 		}
 		
-		cardEffect(smithy, choice1, choice2, choice3, &testG, handpos, &bonus);
+		cardEffect(smithy, 0, 0, 0, &testG, 1, 0);
 		//put played card in played card pile
 		//discardCard(handpos, curPlayer, &testG, 0);
 		
